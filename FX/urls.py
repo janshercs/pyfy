3b11b03bf9e0f2c9acaaad990b3c824c11cb2f3e
Jansen <jansen.cs@gmail.com>
2020-07-05 15:52:15 +0800

    modified images and added overlay

--- FX/urls.py
+++ FX/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'FX'
 urlpatterns = [
     path('', views.index.as_view( ), name = 'index'),
     path('spot/', views.FX.as_view(), name = 'spot'),
     path('chart/', views.chart.as_view(), name = 'chart')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
